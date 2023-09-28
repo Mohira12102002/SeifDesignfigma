225d9f1a3e96a30a74f7be6d3dd069dcba085062
Mohira Normamatova <mohiranormamatova757@gmail.com>
2023-09-28 09:22:23 +0500

    The End

--- main.js
+++ main.js
@@ -6,20 +6,27 @@ const closeNavToggle = document.querySelector('.fa-xmark');
 
 const navLinks = document.querySelector('.nav-links');
 
-const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector('nav');
+
+const heroImg = document.querySelector('.hero-img');
 
 
 closeNavToggle.style.display = 'none';
 
 openNavToggle.addEventListener('click', ()=>{
     navLinks.classList.remove('hidden');
+    navbar.style.position ='absolute';
+    navbar.style.width = '100%';
     navbar.style.height = '400px';
+    navbar.style.background = 'linear-gradient(270deg, #7743DB 31.64%, #5725E5 66.78%)';
     openNavToggle.style.display = 'none';
     closeNavToggle.style.display = 'inline-block';
+    heroImg.style.margin = '100px auto auto auto';
 
 })
 
 closeNavToggle.addEventListener('click', ()=>{
+    console.log(12);
     navLinks.classList.add('hidden');
     navbar.style.height = 'auto';
     openNavToggle.style.display = 'inline-block';
